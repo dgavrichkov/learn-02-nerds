4a882bbecd0375a2f7bc4ba258d67100f30eabaf
DitmarGrim <dgavrichkov32@gmail.com>
2018-07-28 01:12:21 +0300

    запрограммировал слайдер

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 var modal = document.querySelector('.modal');
 var closer = document.querySelector('.modal__close');
 var trigger = document.querySelector('.modal--call');
-console.log('я нашел модалку');
 
 trigger.addEventListener('click', function(evt){
 	evt.preventDefault();
@@ -11,3 +10,32 @@ trigger.addEventListener('click', function(evt){
 closer.addEventListener('click', function(){
 	modal.classList.remove('modal--on');
 });
+
+// slider
+var slideIndex = 1; // это порядковый номер слайда
+showSlides(slideIndex); // это вызов функции, которая показывает слайд
+
+function currentSlide(n){
+	showSlides(slideIndex = n);
+} // это функция, которая вызывается нажатием на переключатель и сама вызывает функцию вызова слайда
+
+function showSlides(n){
+	var i; //определяем счетчик
+	var slides = document.getElementsByClassName('slide'); // составляем массив слайдов
+	var dots = document.getElementsByClassName('slider__control');//собираем массив переключателей
+	if (n > slides.length){
+		slideIndex = 1;
+	}// если функция получила значение, превышающее общее количество слайдов, то показывется самый первый
+	if (n < 1){
+		slideIndex = slides.length;
+	}// если функция получила значение менее одного (эт как?), то нам будет показан последний слайд
+	for(i = 0; i < slides.length; i++){//перебираем слайды
+		slides[i].classList.remove('slide--on');
+	};// цикл проходит по слайдам, отбирая у них видимость
+	for (i = 0; i < dots.length; i++) {
+		dots[i].classList.remove('slider__control--current')
+	};// цикл проходит по переключателям, убирая у них активное состояние
+	slides[slideIndex-1].classList.add('slide--on');// в итоге функция показывает слайд, соответствующий индексу. элементы массива-то нумеруются с нуля
+	dots[slideIndex-1].classList.add('slider__control--current');
+
+}
